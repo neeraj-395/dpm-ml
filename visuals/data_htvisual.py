3120f86f6d807949be16453e4a356e752aa01053
neeraj-395 <nsinghh.04@gmail.com>
2024-08-17 16:16:38 +0530

    minor changes to 'data_htvisual'

--- visuals/data_htvisual.py
+++ visuals/data_htvisual.py
@@ -13,18 +13,22 @@ NUM_PROGNOSIS = 20
 
 # Load the dataset and separate the 'prognosis' column
 data = pd.read_csv('../dataset/testing.csv')
-prognosis = data.pop("prognosis").head(NUM_PROGNOSIS)  # Extract the first 'n' prognosis labels
+
+# Extract the first 'n' prognosis labels
+prognosis = data.pop('prognosis').head(NUM_PROGNOSIS)
 
 # Define the color map for the heatmap: light green for '0' and red for '1'
 cmap = sns.color_palette(['lightgreen', 'red'])
 
 # Generate the heatmap for the selected number of symptoms and prognosis
 sns.heatmap(data.iloc[:NUM_SYMPTOMS, :NUM_PROGNOSIS], cbar=False, linewidths=0.5,
-            cmap=cmap, linecolor="black", yticklabels=prognosis) # type: ignore
+            cmap=cmap, linecolor='black', yticklabels=prognosis) # type: ignore
 
 # Set the axis labels
 plt.xlabel('Symptoms')
 plt.ylabel('Prognosis')
+plt.yticks(fontsize=8)
+plt.xticks(fontsize=8)
 
 # Adjust the layout to ensure proper spacing and prevent overlap
 plt.tight_layout()
